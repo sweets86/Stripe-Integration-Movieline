e08f7c9346f20cbd93445699b580b5df4ea61fe1
MichelleLan90 <michelle.lanaro90@gmail.com>
2020-06-07 16:04:53 +0200

    Cart style

--- src/components/CartView.tsx
+++ src/components/CartView.tsx
@@ -18,18 +18,18 @@ function CartView(props: Props) {
         <CartConsumer>
             {(contextData: ContextState) => {
                 return (
-                    <div style={productsContainer}>
-                        <h1>Cart</h1>
+                    <div style={cartContainer}>
+                        <h1 style={cartTitle}>Cart</h1>
                         {
                             contextData.cartList.length ?
 
                                 contextData.cartList.map((product) => {
                                     return (
-                                        <div style={productCards}>
-                                            <h3 style={TitleLink}>{product.title}</h3>
-                                            <img src={require("./../assets/" + product.img)} alt="pic" style={poster} />
-                                            <h3>{product.price} SEK</h3>
-                                            <h3>Antal (Remove/Add more)</h3>
+                                        <div style={singleCartItem}>
+                                            <h3 style={childrenFlex}>{product.title}</h3>
+                                            <img src={require("./../assets/" + product.img)} alt="pic" style={{...posterInCart, ...childrenFlex}} />
+                                            <h3 style={childrenFlex}>{product.price} SEK</h3>
+                                            <h3 style={childrenFlex}>Antal (Remove/Add more)</h3>
                                         </div>
                                     )
                                 })
@@ -49,12 +49,32 @@ function CartView(props: Props) {
 
 export default CartView
 
-const container: React.CSSProperties = {
-    display: "flex",
-    justifyContent: "center",
-    textAlign: "center",
-    width: "100%",
-    flexDirection: "column",
-    position: "fixed",
-    height: '100%',
+
+const cartTitle: React.CSSProperties = {
+    textAlign: 'center'
+}
+
+const cartContainer: React.CSSProperties = {
+    width: '100%',
+    height: 'auto',
+   
+    
+}
+
+const singleCartItem: React.CSSProperties = {
+    width: '100%',
+    height: '20%',
+    display: 'flex',
+    border: 'grey solid 1px',
+    flexWrap: 'wrap'
+}
+
+const posterInCart: React.CSSProperties = {
+    order: 'initial'
+}
+
+const childrenFlex: React.CSSProperties = {
+    padding: '2%',
+    margin: '2%',
+    width: '200px'
 }
