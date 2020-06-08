46d3023e8b464c44344346478343bcd0a4425a62
Essa <essa992@gmail.com>
2020-06-08 14:12:38 +0200

    Quantity + key adding

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { CSSProperties } from 'react'
-import {Link } from 'react-router-dom'
-import {Alignment, Button, Navbar} from "@blueprintjs/core";
+import { Link } from 'react-router-dom'
+import { Alignment, Button, Navbar } from "@blueprintjs/core";
 import { CartConsumer, ContextState } from '../context/cartContext'
 
 export default function viewNavBar() {
@@ -10,42 +10,42 @@ export default function viewNavBar() {
 
                 <Navbar.Group align={Alignment.LEFT} >
                     <Link to='/'>
-                        <Navbar.Heading  style={styleHeading}>MovieLine</Navbar.Heading>
+                        <Navbar.Heading style={styleHeading}>MovieLine</Navbar.Heading>
                     </Link>
                 </Navbar.Group>
-        
+
                 <Navbar.Group align={Alignment.RIGHT} style={cartContainer}>
 
-                   
-                        <Link to='/cart/'>
-                            <Button className="bp3-button bp3-minimal bp3-icon-shopping-cart" style={cartChildren}/>
-                            <CartConsumer>
-                                {(contextData: ContextState) => { return (<span style={cartChildren}>{contextData.cartList.length}</span>)}}
-                            </CartConsumer>
-                            
-                        </Link>
-                 
+
+                    <Link to='/cart/'>
+                        <Button className="bp3-button bp3-minimal bp3-icon-shopping-cart" style={cartChildren} />
+                        <CartConsumer>
+                            {(contextData: ContextState) => { return (<span style={cartChildren}>{contextData.cartItems.length}</span>) }}
+                        </CartConsumer>
+
+                    </Link>
+
                 </Navbar.Group>
             </Navbar>}
         </div>
     )
 };
 
-const styleNavBar:CSSProperties ={
+const styleNavBar: CSSProperties = {
     backgroundColor: "#212121",
     height: "63PX",
     width: "100%",
     position: "sticky",
 }
 
-const cartChildren:CSSProperties ={
+const cartChildren: CSSProperties = {
     width: '50%',
     color: 'white'
 }
 
-const styleHeading:CSSProperties ={
+const styleHeading: CSSProperties = {
     display: "inline-block",
-    alignItems:"right",
+    alignItems: "right",
     fontWeight: "bold",
     fontSize: "30px",
     color: "#FFFFFF",
@@ -53,7 +53,7 @@ const styleHeading:CSSProperties ={
     marginTop: "17px"
 }
 
-const cartContainer:CSSProperties ={
+const cartContainer: CSSProperties = {
     width: '40%',
     justifyContent: 'flex-end',
     alignItems: 'center',
