595e944656c169d0ddfaf6d991454712b073a428
Essa <essa992@gmail.com>
2020-06-10 15:31:42 +0200

    davids ändringar

--- src/components/checkout-components/FormInfo.tsx
+++ src/components/checkout-components/FormInfo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import ReactDOM from 'react-dom'
 import { render } from "@testing-library/react";
-import { Alert, FormGroup, Label, InputGroup } from "@blueprintjs/core";
+import { Alert, FormGroup, Label, InputGroup, Button } from "@blueprintjs/core";
 
 type FormData = {
   firstName: string;
@@ -24,31 +24,33 @@ const success = () => {
 
 export default function InfoForm() {
   const { register, setValue, handleSubmit, errors } = useForm<FormData>();
-  const onSubmit = handleSubmit(({ firstName, lastName, phone, adress }) => {
+  const onSubmit = handleSubmit((values) => {
+    console.log('SUBMIT:', values)
   });
+
+  console.log({ errors })
 
   return (
     <form id="infos-form" onSubmit={onSubmit}>
       <Label className="bp3-fill">First Name:</Label>
-      <InputGroup className="bp3-fill" name="firstName" ref={register} /> <br />
+      <input className="bp3-fill" name="firstName" ref={register({
+        required: true,
+        pattern: {
+          value: /[a-z]\w+/,
+          message: "Please enter a valid name"
+        }
+      }) as any} /> <br />
+      {errors.firstName?.message}
+
       <Label className="bp3-fill">Last Name:</Label>
       <InputGroup className="bp3-fill" name="lastName" ref={register} /> <br />
       <Label className="bp3-fill">Phone number:</Label>
       <InputGroup className="bp3-fill" name="phone" ref={register} /> <br />
       <Label className="bp3-fill">Address:</Label>
       <InputGroup className="bp3-fill" name="address" ref={register} /> <br />
-      <button style={buttonStyle}
-        type="button"
-        onClick={() => {
-          setValue("lastName", "");
-          setValue("firstName", "");
-          setValue("phone", 0);
-          setValue("adress", "");
-          success()
-        }}
-      >
+      <Button type="submit" style={buttonStyle}>
         Save
-      </button>
+      </Button>
     </form>
   );
 }
