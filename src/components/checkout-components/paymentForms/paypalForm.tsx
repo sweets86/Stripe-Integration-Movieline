9ecf8743f022d26c64db885e3c7120b6c37f0736
yanica svensson <sweets86_@hotmail.com>
2020-06-11 00:28:23 +0200

    paypal

--- src/components/checkout-components/paymentForms/paypalForm.tsx
+++ src/components/checkout-components/paymentForms/paypalForm.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { useForm } from "react-hook-form";
 import { Alert, FormGroup, Label, InputGroup, Button } from "@blueprintjs/core";
 
-
 interface Props {
     form: (form: any) => void
 }
@@ -13,11 +12,31 @@ export default class PaypalForm extends React.Component<Props> {
         super(props)
     }
 
+    emailFiller(input: any) {
+        var reg = new RegExp(input.split('').join('\\w'))
+    }
+
     render() {
         return (
             <div>
-                <img src={require("./../../assets/paypal.png")} alt="Paypal"/>
+                <form style={{ display: 'flex', flexDirection: 'column', width: '20%' }} autoComplete="on">
+                    <label>Email
+                        <input name="Email" type="text" placeholder="you@example.com" autoComplete="on" pattern='text' />
+                    </label>
+                    <label>Mobile
+                        <input name="PhoneNumber" type="text" placeholder="mobilnummer" autoComplete="on" pattern='text' />
+                    </label>
+                    <Button type="submit" value="submit" style={buttonStyle}><a href="https://www.paypal.com/se/signin">Submit</a></Button>
+                </form>
+                
+                <img style={{ maxWidth: '25%' }}
+                    src={require("./paypal.png")} alt="Paypal" />
             </div>
         )
     }
+}
+
+const buttonStyle: React.CSSProperties = {
+    width: '100%',
+    border: '1px, grey'
 }
