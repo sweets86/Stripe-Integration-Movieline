f33039a3992fd607409bd2bab317691b85f128c2
Essa <essa992@gmail.com>
2020-06-11 14:29:53 +0200

    component

--- src/components/checkout-components/Delivery.tsx
+++ src/components/checkout-components/Delivery.tsx
@@ -1,1 +1,20 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Radio } from "@blueprintjs/core"
+
+
+
+
+export default function deliveryMethod() {
+
+    return (
+        <div>
+
+            <Radio label="PostNord" value="one" defaultChecked={true} />
+            <Radio label="DHL" value="two" />
+            <Radio label="Express" value="three" />
+            what is this
+
+        </div>
+    );
+}
